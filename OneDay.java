7a807d838a5fd3ddde7e5432fdb0f4988d8213a6
Parul Saraogi <parulsaraogi@Paruls-MacBook-Air.local>
2025-11-17 01:05:39 +0530

    Initial project upload

---
+++ OneDay.java
@@ -0,0 +1,9 @@
+public class OneDay {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kanha!");
+    }
+}   
+
+
+// This program prints a greeting message to the console.// It serves as a simple demonstration of a Java program structure.// The main method is the entry point of the program where execution begins.
+
