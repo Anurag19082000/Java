9a552e4d679f027c0ee612ece48be3c5fb1cec9d
Parul Saraogi <parulsaraogi@Paruls-MacBook-Air.local>
2025-11-17 01:08:36 +0530

    Updated OneDay

--- OneDay.java
+++ OneDay.java
@@ -7,3 +7,12 @@ public class OneDay {
 
 // This program prints a greeting message to the console.// It serves as a simple demonstration of a Java program structure.// The main method is the entry point of the program where execution begins.
 
+// now to upload this file to github repository.
+
+/*  Run git init to initialize the project,
+ then add your GitHub repository using git remote add origin <repo-url>,
+  stage all files with git add ., commit them using git commit -m "Initial project upload",
+   set the main branch using git branch -M main,
+    and finally upload everything to GitHub with git push -u origin main.
+
+    */
