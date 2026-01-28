9edc0c4270b987d7eb0843e3b4bfbf7f510bdc8d
Parul Saraogi <parulsaraogi@192.168.1.4>
2026-01-29 00:05:07 +0530

    Arraylist

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        System.out.println(nums[2]);
+
+        int number[] =  new int[4];
+        System.out.println(number[2]);
+    }
+}
---
+++ Arraylist.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Anurag");
+        list.add("parul");
+
+        System.out.println(list);
+    }
+}
